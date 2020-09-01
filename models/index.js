2d9d60f5dd91a8cedc552413c83cff34a7603b41
Michelle Tran <mptran0101@berkeley.edu>
2020-08-31 23:35:25 -0700

    created connection.js

---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Role: require("./Role"),
+    Employee: require("./Employee"),
+    Department: require("./Department")
+};
--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.json());
 const apiRoutes = require("./routes/api-routes");
 app.use(apiRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__driname, "client", "build", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 });
