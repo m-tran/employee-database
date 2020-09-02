24ec1eb9b16d47494fc7f98b127ef39d840f88e3
Michelle Tran <mptran0101@berkeley.edu>
2020-09-01 22:56:30 -0700

    make it better

---
+++ client/src/Hooks/getAllEmployees.js
@@ -0,0 +1,13 @@
+import { useEffect } from React;
+import axios from "axios";
+
+const getAllEmployees = (setEmployeeList) => {
+    useEffect(() => {
+        const API = async(setEmployeeList) => {
+            let config = {
+                method: "GET",
+                url: "https://"
+            }
+        }
+    })
+}
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,12 +6,45 @@ router.get("/employees", (req, res) => {
     db.Employee.find({}).then((employees) => res.send(employees));
 });
 
-router.post("/employees", (req, res) => {
-    db.Employee.create(req.body)
+router.get("/employees/:id", (req, res) => {
+    db.Employee.findOne({ id: req.params.id })
     .then(function(dbEmployee) {
-        res.json(dbEmployee);
+        res.send(dbEmployee);
     })
     .catch(function(err) { res.json(err); });
+});
+
+router.post("/employees", (req, res) => {
+    db.Employee.create({ 
+        name: req.body.name,
+        phone: req.body.phone,
+        address: req.body.address,
+        title: req.body.title,
+    })
+    .then(function(dbEmployee) {
+        res.send(dbEmployee);
+    })
+    .catch(function(err) { res.json(err); });
+});
+
+router.delete("/employees/:id", (req, res) => {
+    db.Employee.destroy({ 
+        where: { id: req.params.id }
+    })
+    .then(() => res.send("deleted!"));
+});
+
+router.put("/employees", (req, res) => {
+    db.Employee.update({ 
+        name: req.body.name,
+        phone: req.body.phone,
+        address: req.body.address,
+        title: req.body.title,
+    }, 
+    { 
+        where: { id: req.body.id }
+    })
+    .then(() => res.send("updated!"));
 });
 
 router.get("/departments", (req, res) => {
@@ -32,6 +65,15 @@ router.get("/roles", (req, res) => {
 
 router.post("/roles", (req, res) => {
     db.Role.create(req.body)
+    .then(function(dbRole) {
+        res.json(dbRole);
+    })
+    .catch(function(err) { res.json(err); });
+});
+
+router.get("/roles/:id", (req, res) => {
+    db.Role.findOne({ _id: req.params.id })
+    .populate("department_id")
     .then(function(dbRole) {
         res.json(dbRole);
     })
