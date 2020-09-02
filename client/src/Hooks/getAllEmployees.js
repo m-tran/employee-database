c31cf890e5f5c4af6d1ce482fae42f76e8ffda92
Michelle Tran <mptran0101@berkeley.edu>
2020-09-01 23:01:39 -0700

    added hook

--- client/src/Hooks/getAllEmployees.js
+++ client/src/Hooks/getAllEmployees.js
@@ -6,8 +6,32 @@ const getAllEmployees = (setEmployeeList) => {
         const API = async(setEmployeeList) => {
             let config = {
                 method: "GET",
-                url: "https://"
-            }
-        }
-    })
-}
+                url: "https://employee-db-22.herokuapp.com/employees",
+                headers: {},
+            };
+
+            await axios(config)
+            .then((res) => {
+                let jsonString = JSON.stringify(res.data);
+                let jsonObject = JSON.parse(jsonSTring);
+
+                const employeesMapped = jsonObject.map((employee) => {
+                    return {
+                        name: employee.name,
+                        phone: employee.phone,
+                        address: employee.address,
+                        title: employee.title,
+                        id: employee.id,
+                    };
+                });
+                let employeeData = { data: employeesMapped };
+                
+                setEmployeeList(employeeData);
+            })
+            .catch((error) => console.log(error));
+        };
+        API(setEmployeeList);
+    }, [setEmployeeList]);
+};
+
+export default getAllEmployees;
